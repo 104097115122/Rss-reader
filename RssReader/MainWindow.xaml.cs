69f1b5580fca59a74fc1766035953b747af4ae73
George-dev12 <sgugniewicz@poczta.fm>
2022-11-27 17:35:46 +0100

    Add link to recent links list

--- RssReader/MainWindow.xaml.cs
+++ RssReader/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using System.ServiceModel.Syndication;
 using System.Xml;
 using System.Diagnostics;
+using System.IO;
 
 namespace RssReader
 {
@@ -26,6 +27,22 @@ namespace RssReader
         public MainWindow()
         {
             InitializeComponent();
+            //pobranie wartosci z pliku z zapisanymi linkami
+            try
+            {
+                using (StreamReader sr = new StreamReader(@"D:\ProjektWPF\Rss-reader\lista.txt")) 
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null) 
+                    {
+                        LinkList.Items.Add(line);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Could not find the file!");
+            }
         }
 
         private void AddLink_Click(object sender, RoutedEventArgs e)
@@ -40,30 +57,46 @@ namespace RssReader
 
         private void AddLink1_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                XmlReader reader = XmlReader.Create(AddLink.Text); // Reads a link from TextBox
+            //try
+            //{
+            //    XmlReader reader = XmlReader.Create(AddLink.Text); // Reads a link from TextBox
 
-                // Change of settings in order to fix DTD issue
-                XmlReaderSettings readerSettings = new XmlReaderSettings();
-                readerSettings.DtdProcessing = DtdProcessing.Parse;
+            //    // Change of settings in order to fix DTD issue
+            //    XmlReaderSettings readerSettings = new XmlReaderSettings();
+            //    readerSettings.DtdProcessing = DtdProcessing.Parse;
 
-                SyndicationFeed feed = SyndicationFeed.Load(reader);
+            //    SyndicationFeed feed = SyndicationFeed.Load(reader);
 
-                //Reads a Title 
-                foreach (SyndicationItem item in feed.Items)
-                {
-                    Content.Text = item.Title.Text.ToString();
-                }
+            //    //Reads a Title 
+            //    foreach (SyndicationItem item in feed.Items)
+            //    {
+            //        Content.Text = item.Title.Text.ToString();
+            //    }
 
-            }
-            catch (Exception)
-            {
+            //}
+            //catch (Exception)
+            //{
 
-                throw;
-            }
+            //    throw;
+            //}
         }
 
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
 
+        }
+
+        private void Search(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(Link.Text);
+        }
+
+        private void SaveLinkToFile(object sender, RoutedEventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter(@"D:\ProjektWPF\Rss-reader\lista.txt")) 
+            {
+                sw.WriteLine(Link.Text); 
+            }
+        }
     }
 }
