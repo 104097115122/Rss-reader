1048219eff291204f5917f20af40c7276b49c9ea
George-dev12 <sgugniewicz@poczta.fm>
2022-12-04 15:56:45 +0100

    UI version beta

--- RssReader/MainWindow.xaml.cs
+++ RssReader/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Collections.ObjectModel;
 using Path = System.IO.Path;
+using System.Security;
 
 namespace RssReader
 {
@@ -40,12 +41,12 @@ namespace RssReader
             {
                 using (StreamWriter sw = new StreamWriter(dir))
                 {
-                    sw.WriteLine(Link.Text);
+                    sw.WriteLine(linkTextBox.Text);
                 }
             }
             else
             {
-                File.AppendAllText(dir, $"\n{Link.Text}");
+                File.AppendAllText(dir, $"\n{linkTextBox.Text}");
             }
 
             ReadFromFile();
@@ -62,7 +63,7 @@ namespace RssReader
                 using (StreamReader sr = new StreamReader(dir))
                 {
                     string line;
-                    LinkList.ItemsSource = links;
+                    linkList.ItemsSource = links;
                     while ((line = sr.ReadLine()) != null)
                     {
                         links.Add(line);
@@ -77,55 +78,70 @@ namespace RssReader
 
         private void SearchLink(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                WriteTitle(Link.Text);
+            //try
+            //{
+            //    WriteTitle(Link.Text);
 
-            }
-            catch (Exception)
-            {
+            //}
+            //catch (Exception)
+            //{
 
-                MessageBox.Show("Could not find the website!");
-            }
+            //    MessageBox.Show("Could not find the website!");
+            //}
         }
 
         private void LinkList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            try
-            {
-                if (LinkList.SelectedIndex >= 0)
-                {
-                    //Take selected link from list of links as link
-                    dynamic link = LinkList.SelectedItem as dynamic;
-                    
+            //try
+            //{
+            //    if (LinkList.SelectedIndex >= 0)
+            //    {
+            //        //Take selected link from list of links as link
+            //        dynamic link = LinkList.SelectedItem as dynamic;
 
-                    WriteTitle(link);
-                    
-                }
 
-            }
-            catch (Exception)
-            {
+            //        WriteTitle(link);
 
-                MessageBox.Show("Could not find the website!");
-            }
+            //    }
+            //}
+            //catch (Exception)
+            //{
+
+            //    MessageBox.Show("Could not find the website!");
+            //}
         }
 
         private void WriteTitle(string link)
         {
-            XmlReader reader = XmlReader.Create(link); // Reads a link from TextBox
+            //XmlReader reader = XmlReader.Create(link); // Reads a link from TextBox
 
-            // Change of settings in order to fix DTD issue
-            XmlReaderSettings readerSettings = new XmlReaderSettings();
-            readerSettings.DtdProcessing = DtdProcessing.Parse;
+            //// Change of settings in order to fix DTD issue
+            //XmlReaderSettings readerSettings = new XmlReaderSettings();
+            //readerSettings.DtdProcessing = DtdProcessing.Parse;
 
-            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            //SyndicationFeed feed = SyndicationFeed.Load(reader);
 
-            //Reads a Title 
-            foreach (SyndicationItem item in feed.Items)
-            {
-                Content.Text = item.Title.Text.ToString();
-            }
+            ////Reads a Title 
+            //foreach (SyndicationItem item in feed.Items)
+            //{
+            //    Content.Text = item.Title.Text.ToString();
+            //}
+        }
+
+        private void ClearPlaceholder(object sender, RoutedEventArgs e)
+        {
+            linkTextBox.FontWeight = FontWeights.Normal;
+            linkTextBox.FontStyle = FontStyles.Normal;
+            linkTextBox.Text = string.Empty;
+        }
+
+        private void ButtonClicked(object sender, RoutedEventArgs e)
+        {
+            if (linkTextBox.Text != "Enter a link!")
+                SaveLinkToFile(sender, e);
+            linkTextBox.FontWeight = FontWeights.ExtraLight;
+            linkTextBox.FontStyle = FontStyles.Italic;
+            linkTextBox.Text = "Enter a link!";
         }
     }
 }
